90d6300e4a84f8432d1f302787c0581619ad8c4f
EiIoGo <111787747+EiIoGo@users.noreply.github.com>
2024-05-30 09:39:20 -0300

    Initial commit

---
+++ Assets/PasswordComprobador.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PasswordComprobador : MonoBehaviour
+{
+    const string pass = "12345";
+
+    public InputField password;
+    public Button boton;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void Presionar()
+    {
+        if(password.text == pass)
+        {
+            Debug.Log("Access granted");
+        }
+        else
+        {
+            Debug.Log("Access denied");
+        }
+    }
+}
